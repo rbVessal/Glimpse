616d10928f06926a61fcac00d89477b168c6a265
Amanda <ani6036@rit.edu>
2014-02-01 17:57:25 -0500

    Image picker almost works.

---
+++ glimpse/glimpse/TutorialViewController.m
@@ -0,0 +1,29 @@
+//
+//  TutorialViewController.m
+//  glimpse
+//
+//  Created by Student on 2/1/14.
+//  Copyright (c) 2014 Student. All rights reserved.
+//
+
+#import "TutorialViewController.h"
+
+@interface TutorialViewController ()
+
+@end
+
+@implementation TutorialViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
